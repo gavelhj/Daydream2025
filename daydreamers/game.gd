9dcfac46e30e04537a0c082f528a0c9f693cceb9
gavelhj <tgvbhjj2@gmail.com>
2025-09-28 10:34:08 +1000

    bruh

--- daydreamers/game.gd
+++ daydreamers/game.gd
@@ -9,7 +9,10 @@ func night_check():
 		begin_level("night")
 
 func begin_level(time): #day or night
-	pass
+	Scene.changeTo("res://World.tscn")
+	var player = load("res://Player.tscn")
+	player.instantiate()
+	player.position = Vector2.ZERO
 
 func game_loop_start():
 	pass
